4488c31bff63a8ecf67e84f383b3e4b166ab29a7
swaroop.srinivasan <swaroop.srini@gmail.com>
2019-01-28 08:32:40 +0530

    cli project initialisation commit

---
+++ client/src/pages/LandingPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Landing Page
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "landing-page"
+  }
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import LandingPage from './pages/LandingPage'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'LandingPage',
+      component: LandingPage
+    }
+  ]
+})
